49cb37877c02ffd31840383700a1504c6877c3f0
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-07-22 19:24:47 -0300

    manos a la obra 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { TasksListModule } from './tasks-list/tasks-list.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    TasksListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
